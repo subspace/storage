67f9e7b1724dc6f7bf5e48ec0cab2a8914b715fb
Jeremiah Wagstaff <jeremiah.wagstaff@gmail.com>
2018-09-18 01:37:15 -0700

    compiled

--- dist/browser_storage.js
+++ dist/browser_storage.js
@@ -8,7 +8,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const browser_storage = require('local-forage');
+const browser_storage = require('localforage');
 const Adapter = {
     put: (key, value) => __awaiter(this, void 0, void 0, function* () {
         try {
